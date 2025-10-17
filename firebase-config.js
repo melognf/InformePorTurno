4c41bf949c21f4b427ef5add47c94201f2038db7
melognf <melognf@yahoo.com.ar>
2025-10-17 19:56:42 -0300

    comunicacion

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,16 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
+// 🔹 Configuración Firebase (revisada)
 const firebaseConfig = {
   apiKey: "AIzaSyDeTI8mHF5QkNEbgu69L5JMqui-aimFlFM",
   authDomain: "informeporturno.firebaseapp.com",
   projectId: "informeporturno",
-  storageBucket: "informeporturno.firebasestorage.app",
+  storageBucket: "informeporturno.appspot.com",   // ✅ corregido
   messagingSenderId: "1080632095129",
   appId: "1:1080632095129:web:0f1c6abebecc30022a6308"
 };
 
+// 🔹 Inicialización
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
